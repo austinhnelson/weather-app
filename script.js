176b14a9f2273d8ad7751fe177b270619d742639
Austin Nelson <nelsonhaustin@gmail.com>
2023-09-23 12:46:15 -0600

    Finished Front-End GUI

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiKey = 'dc6d5ae5685231f2230e2d18683c12d1';
+const loookupButton = document.querySelector('.lookup-button');
+const todayInfo = document.querySelector('.todays-information');
+const todayWeatherIcon = document.querySelector('.todays-weather-icon');
+const todayTemp = document.querySelector('.todays-temp');
+const daysList = document.querySelector('.days-list');
+
